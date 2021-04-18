a7bd4009a94628d571676989d2131c9a1440a8da
Emmett Rushin <emmettrushin@gmail.com>
2021-04-18 17:25:35 -0400

    added timer with js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
 var quiz = document.getElementById('quiz')
 var startButton = document.getElementById('startButton')
+var timer = document.getElementById('timer')
+var secondsLeft = 75; 
 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent= "Time:" + secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval)
+        }
+    }, 75000)
+}
