44ca986d60f95f5fc9d1c9b4c22ce40d87931697
Emmett Rushin <emmettrushin@gmail.com>
2021-04-22 17:44:31 -0400

    attempting to fix storage bug

--- assets/score.js
+++ assets/score.js
@@ -1,5 +1,9 @@
 var highScore = document.getElementById('highScore')
 var userScore = localStorage.getItem('userScore')
 var initials = localStorage.getItem('initials')
+var scores = document.createElement('li');
+// var hs = localStorage.setItem(highScore)
 
 document.getElementById('highScore').textContent = (userScore + initials)
+score.appendChild(document.createTextNode(userScore + initials));
+document.querySelector('ol').appendChild(scores);
