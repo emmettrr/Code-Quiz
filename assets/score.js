48f6db8bed6b5d89f2b309e2ba64d32f9fd07e70
Emmett Rushin <emmettrushin@gmail.com>
2021-04-23 16:52:02 -0400

    fixed function and html in hs and score.js

--- assets/score.js
+++ assets/score.js
@@ -1,9 +1,40 @@
-var highScore = document.getElementById('highScore')
-var userScore = localStorage.getItem('userScore')
-var initials = localStorage.getItem('initials')
-var scores = document.createElement('li');
-// var hs = localStorage.setItem(highScore)
+var endScores = JSON.parse(localStorage.getItem('scores'));
+var resetHStn = document.getElementById('clear');
+var tryAgainBtn = document.getElementById('tryAgain');
 
-document.getElementById('highScore').textContent = (userScore + initials)
-score.appendChild(document.createTextNode(userScore + initials));
-document.querySelector('ol').appendChild(scores);
+//creates new list items and places high scores and initials from local storage into said list. 
+if (endScores === null){
+}else{
+for (let index = 0; index < endScores.length; index++) {
+    var a = document.createElement('li');
+    var b = document.createTextNode(`${index + 1}. ${endScores[index].playerInitials.toUpperCase()} - ${endScores[index].userScore}`);
+    a.appendChild(b);
+    document.querySelector('#highScore').appendChild(a);
+    a.className += 'userList';
+}}
+
+//compares current score with new incoming score and sets
+function betterThan(a,b){
+    if (a.userScore < b.userScore){
+        return 1;
+    }
+    if (a.userScore > b.userScore){
+        return -1;
+    }
+    return 0;
+}
+//takes you back to homepage to take quiz again
+if (tryAgainBtn === null){
+}else{
+tryAgainBtn.addEventListener('click', function(event){
+    console.log(event)
+    window.location.replace("../index.html")
+})};
+// resets the local storage, will display no old high scores
+if(resetHStn === null){
+
+}else{
+resetHStn.addEventListener('click', function(){
+    localStorage.clear();
+    document.getElementById('playerList').innerHTML = ""
+})};
